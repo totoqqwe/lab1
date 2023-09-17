c780e4a6f3cf46f629ed76e4a53a3a7c1df26742
totoqqwe <anton.stepanchenko14@gmail.com>
2023-09-17 13:01:01 +0300

    add change in new branch2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("qweqwe");
+            System.Console.WriteLine("qweqweqwe");
         }
     }
 }
